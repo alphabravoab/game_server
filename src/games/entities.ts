97a278ef088b287c265420b74be007d95a27e037
Arjen <alphabravoab@gmail.com>
2018-07-13 14:28:37 +0200

    userid

--- src/games/entities.ts
+++ src/games/entities.ts
@@ -56,8 +56,8 @@ export class Player extends BaseEntity {
   @ManyToOne(_ => Game, game => game.players)
   game: Game
 
-  @Column()
-  userId: number
+  // @Column()
+  // userId: number
 
   @Column('char', {length: 1})
   symbol: Symbol
