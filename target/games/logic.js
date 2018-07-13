2faa6b7eb83d388c848679246365aebbaf4c8ca4
Arjen <alphabravoab@gmail.com>
2018-07-13 14:25:51 +0200

    added target

---
+++ target/games/logic.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.attack = (attacker, defender) => {
+    return defender.health = defender.health - attacker.attack;
+};
+//# sourceMappingURL=logic.js.map
